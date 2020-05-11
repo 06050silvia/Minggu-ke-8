368c4d9d1d36aebd5c585ca2142b8773252d57ab
Your Name <112201806050@mhs.dinus.ac.id>
2020-05-11 08:52:14 +0700

    ribet banget

---
+++ menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<div class="menu">
+		<ul>
+			<li><a href="index.php?p=home">Home</a></li>
+			<li><a href="index.php?p=sekolah">Sekolah</a></li>
+		</ul>
+		</div>
+	</body>
+</html>
